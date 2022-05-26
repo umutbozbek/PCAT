5502f7cc9dba0925b2c97f66e6c452fb0e04bec7
umutbozbek <umutbozbek58@gmail.com>
2022-05-26 18:52:50 +0300

    nodemon kurulumu

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = function () {
-    console.log('fonksiyon working');
-}
+const express = require('express')
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    const photo={
+        id:1,
+        name:"photo name",
+        description:"photo description"
+    }
+    res.send(photo)
+})
+
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı`);
+})
