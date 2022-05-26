c0a1d62bee1a59ee7a9c255c03864fbc4bcfeba8
umutbozbek <umutbozbek58@gmail.com>
2022-05-26 19:59:40 +0300

    Static Files

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
+const path = require('path')
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    const photo={
-        id:1,
-        name:"photo name",
-        description:"photo description"
-    }
-    res.send(photo)
+// const myLogger=(req,res,next)=>{
+//     console.log('middleware log 1');
+//     next();
+// }
+
+// const myLogger2=(req,res,next)=>{
+//     console.log('middleware log 2');
+//     next();
+// }
+
+
+//MİDDLEWARES
+app.use(express.static('public'))
+// app.use(myLogger)
+// app.use(myLogger2)
+
+
+
+app.get('/', (req, res,) => {
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
 })
 
 
