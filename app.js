a322734a0407fbfb4335602e4474ad1b4ec4cb19
umutbozbek <umutbozbek58@gmail.com>
2022-06-02 19:18:11 +0300

    PCAT

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const path = require('path');
-const ejs=require('ejs');
-const Photo=require('./models/Photo');
-const { emitWarning } = require('process');
+const ejs = require('ejs');
+const fs = require('fs')
+const Photo = require('./models/Photo');
+
 
 
 const app = express();
@@ -12,34 +14,33 @@ const app = express();
 mongoose.connect('mongodb://0.0.0.0:27017/pcat-test-db')
 
 //Tamplate ENGINE
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 
 
 
 
 //MİDDLEWARES
 app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
+app.use(fileUpload());
 
 //Rautes
 app.get('/', async (req, res,) => {
-  const photos=await Photo.find({})
-  res.render('index',{
+  const photos = await Photo.find({}).sort('-dateCreated')
+  res.render('index', {
     photos
   })
 })
 
-app.get('/photos/:id', async(req, res,) => {
-  // console.log(req.params.id);
-  //res.render('about')
-  const photo=await Photo.findById(req.params.id)
-  res.render('photo',{
+app.get('/photos/:id', async (req, res,) => {
+
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo', {
     photo
   })
 })
-
+   
 
 app.get('/about', (req, res,) => {
   res.render('about')
@@ -49,13 +50,29 @@ app.get('/add', (req, res,) => {
 })
 
 app.post('/photos', async (req, res,) => {
-Photo.create(req.body)
-  res.redirect('/')
+
+
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath,
+    async () => {
+      await Photo.create({
+        ...req.body,
+        image: '/uploads/' + uploadeImage.name,
+      })
+      res.redirect('/')
+    })
 });
 
 
 const port = 3000
 
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda başlatıldı`);
+  console.log(`Sunucu ${port} portunda başlatıldı`);
 })
